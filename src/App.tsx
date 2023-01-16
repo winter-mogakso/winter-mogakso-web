b127e4dc6ab4fef5a46a03dd03134799daea0300
KANG DONGHA <knamuuu@naver.com>
2023-01-16 23:45:33 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <div>React and TS without CRA</div>
+            <div>Prettier and eslint</div>
+        </div>
+    );
+}
+
+export default App;
