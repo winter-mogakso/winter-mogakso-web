1496c9ec8fa4cfcc1c2d7b37c33ae511c3992e36
KANG DONGHA <knamuuu@naver.com>
2023-01-19 23:59:18 +0900

    Feat: RecoilRoot 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <BrowserRouter>
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
+    </BrowserRouter>,
+    rootElement
+);
