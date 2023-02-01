bd918a7df0c3053590a28d9b4f0c4581d35832e2
KANG DONGHA <knamuuu@naver.com>
2023-02-01 22:36:34 +0900

    Design: 메인 페이지 디자인 수정

--- src/components/organisms/Home/Content/index.tsx
+++ src/components/organisms/Home/Content/index.tsx
@@ -8,10 +8,14 @@ function Content() {
     return (
         <div className={styles.content}>
             <div className={styles.content_left}>
-                <div>모각소에서 화상 회의, 화면 공유 그리고 실시간 채팅을 경험해보세요!</div>
-                <button className={styles.button}>새 회의 시작</button>
+                <div>
+                    <div>모각소에서 화상 회의, 화면 공유 그리고 실시간 채팅을 경험해보세요!</div>
+                    <button className={styles.button}>새 회의 시작</button>
+                </div>
             </div>
-            <Mogakso />
+            <div className={styles.content_right}>
+                <Mogakso />
+            </div>
         </div>
     );
 }
--- src/components/organisms/Layout/Header/index.tsx
+++ src/components/organisms/Layout/Header/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import HeaderTabs from 'src/components/molecules/HeaderTabs';
 
 function Header() {
     return (
         <div className={styles.header}>
             <div className={styles.Logo}>MOGAKSO</div>
-            <HeaderTabs />
             <div className={styles.account_btn}>
                 <div>Login</div>
                 <div>Sign up</div>
